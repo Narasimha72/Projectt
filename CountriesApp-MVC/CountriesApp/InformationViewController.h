ba9fc800b7f030216d4bd4046df83ec32486fa42
Nuvvala <narasimha.nuvvala@gmail.com>
2015-07-16 00:09:25 -0500

    added mvc

---
+++ CountriesApp-MVC/CountriesApp/InformationViewController.h
@@ -0,0 +1,16 @@
+//
+//  InformationViewController.h
+//  CountriesApp
+//
+//  Created by Nuvvala on 6/9/15.
+//  Copyright (c) 2015 Narasimha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InformationViewController : UIViewController
+
+@property(nonatomic,strong) IBOutlet UIImageView *imageView;
+@property(nonatomic,strong) UIImage *image;
+
+@end
